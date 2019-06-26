5f9d5b442a49916b833a53a723d9a0ff957e4840
Thulio Carvalho <thulio.carvalho@gmail.com>
2019-06-26 16:16:31 -0300

    Arquivo server.js modificado

--- casadocodigo/server.js
+++ casadocodigo/server.js
@@ -3,6 +3,19 @@
 const http = require ("http"); //Importando um módulo HTTP
 
 //Método de criação do servidor
-const servidor = http.createServer();
-//Fazendo servidor escutar a porta 3000
+//req = requisição
+//resp = resposta
+const servidor = http.createServer(function (req, resp) {
+    resp.end(`
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Casa do Código </h1>
+                </body> 
+            </html>
+    `)
+});
+//Fazendo servidor escutar a porta 3000 
 servidor.listen(3000)
